3c2521f40247459a02ad97c3c1ae1450e067a147
code49 <davidlechan@gmail.com>
2021-06-21 00:17:10 -0700

    Added functions for song images, current time

--- playback.py
+++ playback.py
@@ -1,11 +1,11 @@
 """ 
-This file comprises functions that have to do with data on currently playing music. 
+This file comprises of functions that have to do with data on currently playing music. 
 """
 
 def basic_song_info(spObject):
     """ 
     Return basic data for currently playing song. This is intended for use with 
-    programs displaying the currently playing song. 
+    programs displaying the currently playing song and associated data. 
 
     Parameters 
     ----------
@@ -14,17 +14,16 @@ def basic_song_info(spObject):
 
     Returns
     -------
-    songID: str 
-        The Spotify song id as a string
     artist: str 
         The artist name as a string 
     songName: str 
         The name of the song as a string 
     albumName: str 
         The name of the album as a string 
-    albumCoverURL: str 
-        URL for the cover image of the song album 
-
+    songID: str 
+        The Spotify song id as a string
+    isPlaying: bool
+        Boolean denoting whether Spotify is currently playing or not
     """
 
     trackInfo = spObject.current_user_playing_track() #get track info for current track
@@ -33,7 +32,87 @@ def basic_song_info(spObject):
     artist = trackInfo["item"]["artists"][0]["name"] #get first artist listed 
     songName = trackInfo["item"]["name"] #get song name
     albumName = trackInfo["item"]["album"]["name"] #get album name
-    albumCoverURL = trackInfo['item']['album']['images'][0]['url'] #get album image url
-    songID = trackInfo['item']['id']
+    
+    songID = trackInfo['item']['id'] #get song's track id
+    isPlaying = trackInfo['item']['is_playing'] #get song's current play status
 
-    return ((artist, songName, albumName, albumCoverURL, songID))
+    return ((artist, songName, albumName, songID, is_playing))
+
+def song_image_info(spObject):
+    """
+    Return data around the currently playing song's album image and artist image.
+    This includes information on the various images' size and url.
+
+    Parameters
+    ----------
+    spObject: Spotipy API Object
+        Spotipy object with the scope of: 'user-read-currently-playing'
+
+    Returns
+    -------
+    albumImageData: list
+        List containing dictionaries for all available images for the given album. Each dictionary has the format
+        {height: height, url: url, width:width}
+
+    artistImageData: list
+        List containing dictionaries for all available images for the first listed artist. Each dictionary has the 
+        format {height: height, url:url, width: width}
+    """
+
+    trackInfo = spObject.current_user_playing_track() #get track info for current track
+
+    albumImageData = trackInfo['item']['album']['images'] #get album image list
+    artistImageData = spotify_object.search(q="artist:" + trackInfo['item']['artists'][0]["name"], type="artist")["artists"]["items"][0]["images"] #get artist image list
+
+def playback_time_info(spObject, format="min-sec"):
+    """
+    Return data about the current time in the currently playing song.
+
+    Parameters
+    ----------
+    spObject: Spotipy API Object
+        Spotipy object with the scope of: 'user-read-currently-playing'
+
+    format: string
+        "min-sec" or "ms" - output string format
+
+    Returns
+    -------
+    currentProgress: string
+        String of the current position in the song, formatted according to format.
+
+    totalLength: string
+        String of the total length of the song, formatted according to format.
+    """
+
+    trackInfo = spObject.current_user_playing_track() #get track info for current track
+
+    #yes, I could technically do this more effeciently using a function for time conversion, but I didn't want to write all the stuff introducing a function. sue me.
+
+    #get the current progress and total track length
+    currentProgress = trackInfo["progress_ms"]
+    totalLength = trackInfo["item"]["duration_ms"]
+
+    #if requested, convert the times to minute:second format
+    if format == "min-sec":
+        
+        #converting time for currentProgress
+        currentProgress = currentProgress/1000
+        minutes, seconds = round(currentProgress/60), round(currentProgress - round(currentProgress/60)*60)
+        if seconds < 10 and seconds >= 1:
+            seconds = "0" + str(seconds)
+        elif seconds < 1:
+            seconds = "00"
+        currentProgress = f"{minutes}:{seconds}"
+
+        #converting time for totalLength
+        totalLength = totalLength/1000
+        minutes, seconds = round(totalLength/60), round(totalLength - round(totalLength/60)*60)
+        if seconds < 10 and seconds >= 1:
+            seconds = "0" + str(seconds)
+        elif seconds < 1:
+            seconds = "00"
+        totalLength = f"{minutes}:{seconds}"
+    
+    #return strings as formatted
+    return currentProgress, totalLength
