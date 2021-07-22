33352a7bc9eeea3bb101e79d67afbd25e9ae4a36
TheSharkhead2 <theorodester@gmail.com>
2021-07-22 00:51:51 -0700

    added __init__.py which allows for the class Spotipy2 to be imported straight from the SpotipyFunctionSet module

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+init file for module allowing for Spotipy2 class to be imported straight from
+module, instead of through the mainClass file. 
+
+Citation on this method: https://stackoverflow.com/questions/49694063/expose-export-class-instead-of-module-from-python-file
+
+"""
+
+from .mainClass import Spotipy2
